54a1b3a770fd04219977a9350fe51662eca6b0a4
nirmaldavis <nirmal.davis@gmail.com>
2016-04-12 14:14:24 +0530

    Added tests and Re-factored the code
    Used @Profile to make more testable

--- HelloSpring/src/main/java/com/nirmal/spring/hello/impl/GreetingImpl.java
+++ HelloSpring/src/main/java/com/nirmal/spring/hello/impl/GreetingImpl.java
@@ -3,12 +3,25 @@ package com.nirmal.spring.hello.impl;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.nirmal.spring.hello.api.Greeting;
 
-@Component("greeting")
+@Component
 public class GreetingImpl implements Greeting {
+
+	@Override
+	public String getMessage() {
+		return "Welcome to Spring";
+	}
+
+}
+
+@Component
+@Profile("test1")
+class GreetingImpl1 implements Greeting {
 
 	private Person person;
 	
@@ -22,7 +35,7 @@ public class GreetingImpl implements Greeting {
 	}
 
 	//Method injection 
-	@Autowired(required=false)
+	@Autowired
 	public void setPerson(Person person) {
 		this.person = person;
 	}
@@ -30,11 +43,154 @@ public class GreetingImpl implements Greeting {
 }
 
 @Component
+@Profile("test2")
+class GreetingImpl2 implements Greeting {
+
+	//Field injection
+	@Autowired
+	private Person person;
+	
+	@Override
+	public String getMessage() {
+		return "Welcome to Spring, " + person.getName();
+	}
+}
+
+
+@Component
+@Profile("test3")
+class GreetingImpl3 implements Greeting {
+
+	private Person person;
+
+	//TODO:How Spring can create bean even if constructor is private ?!! 
+	//Ans : ctor.setAccessible(true) - 
+	//Refer @ http://stackoverflow.com/questions/5430605/how-is-spring-beanfactory-able-to-instantiate-a-non-public-class
+	
+	//Constructor injection
+	@Autowired
+	private GreetingImpl3(Person person) 
+	{
+		System.out.println("GreetingImpl3.GreetingImpl3()");
+		this.person = person;
+	}
+	
+	@Override
+	public String getMessage() {
+		return "Welcome to Spring, " + person.getName();
+	}
+
+}
+
+@Component
+@Profile("test4")
+class GreetingImpl4 implements Greeting {
+
+	//Field injection
+	@Autowired(required=false)
+	private Person person;
+	
+	@Override
+	public String getMessage() {
+		return "Welcome to Spring, " + person.getName();
+	}
+
+}
+
+@Component
+@Profile("test5")
+class GreetingImpl5 implements Greeting {
+
+	//Field injection
+	@Autowired(required=false)
+	private Person person;
+	
+	@Override
+	public String getMessage() {
+		String message = "Welcome to Spring";
+		if(person != null) {
+			message = "Welcome to Spring, " + person.getName();
+		}
+		return message;
+	}
+
+}
+
+@Component
+@Profile("test6")
+class GreetingImpl6 implements Greeting {
+
+	
+	private Person person;
+
+	
+	//TODO: If constructor injection needs to work with required=false, need an empty default constructor. 
+	//Otherwise bean cannot be created by spring, why ?!!
+	//Reference @ http://blog.bltavares.com/2013/08/19/following-the-error-spring-autowired-and-bean/
+	
+	
+	@Deprecated
+	private GreetingImpl6()
+	{
+		
+	}
+	
+	//Constructor injection
+	@Autowired(required=false)
+	public GreetingImpl6(Person person)
+	{
+		System.out.println("GreetingImpl6.GreetingImpl6(Person ) ctor");
+		this.person = person;
+	}
+	
+	@Override
+	public String getMessage() {
+		String message = "Welcome to Spring";
+		if(person != null) {
+			message = "Welcome to Spring, " + person.getName();
+		}
+		return message;
+	}
+
+}
+
+
+
+@Component
+//You can specify multiple profiles
+@Profile({"test1","test2","test3"})
 class Person {
 	
 	String name = "Nirmal Davis";
 
-	//Filed injection
+	public String getName() {
+		return name;
+	}
+
+}
+
+
+@Component("Greeting")
+@Profile({"test11", "test12", "test13"})
+class GreetingImpl11 implements Greeting {
+
+	//Field injection
+	@Autowired
+	private Person person;
+	
+	@Override
+	public String getMessage() {
+		return "Welcome to Spring, " + person.getName();
+	}
+}
+
+@Component("Person")
+@Profile({"test11","test12"})
+class Person2 extends Person {
+	
+	String name = "Nirmal Davis";
+
+	//Field injection
 	@Autowired
 	Optional<Address> address;
 	
@@ -47,13 +203,35 @@ class Person {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-	
 }
 
-//@Component
+
+@Component("Person")
+@Profile({"test13"})
+class Person3 extends Person {
+	
+	String name = "Nirmal Davis";
+
+	//Field injection
+	@Autowired
+	@Qualifier("officialAddress")
+	Optional<Address> address;
+	
+	public String getName() {
+		
+		//Execute if Address is present
+		address.ifPresent(consumer->{
+			name = name + " @ " + consumer.getLine();
+		});
+		return name;
+	}
+
+}
+
+
+
+@Component 
+@Profile("test11")
 class Address {
 	String line;
 	
@@ -72,5 +250,13 @@ class Address {
 
 	public void setLine(String line) {
 		this.line = line;
+	}
+}
+
+@Component("officialAddress")
+@Profile("test13")
+class OfficeAddress extends Address {
+	public OfficeAddress() {
+		super("White Field");
 	}
 }
--- HelloSpring/src/test/java/com/nirmal/spring/hello/AnnotationConfigTest.java
+++ HelloSpring/src/test/java/com/nirmal/spring/hello/AnnotationConfigTest.java
@@ -1,9 +1,12 @@
 package com.nirmal.spring.hello;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 
@@ -12,27 +15,189 @@ import com.nirmal.spring.hello.conf.GreetingAnnotationConfig;
 
 public class AnnotationConfigTest {
 
-	private ApplicationContext context;
-
-	@Before
-	public void setUp() throws Exception {
-		context = SpringApplication.run(GreetingAnnotationConfig.class);
-	}
+//	private ApplicationContext context;
+//
+//	@Before
+//	public void setUp() throws Exception {
+//		context = SpringApplication.run(GreetingAnnotationConfig.class);
+//	}
 
 	@Test
-	public void testGreeting() {
+	public void testGettingBeanByAnnotationConfig() {
+		
+		System.clearProperty("spring.profiles.active");
+		
+		ApplicationContext context = SpringApplication.run(GreetingAnnotationConfig.class);
 		Greeting greeting = context.getBean(Greeting.class);
 		String message = greeting.getMessage();
-		assertEquals("Welcome to Spring, Nirmal Davis", message);
+		assertEquals("Welcome to Spring", message);
 	}
 	
 	@Test
 	public void testBeanIdDerivedFromClassNameGreetingImpl() {
+		
+		System.clearProperty("spring.profiles.active");
+		
 		//Testing Bean id derived from class name
 		//the first letter of class name becomes small letter when it becomes bean id
-		Greeting greeting = context.getBean("greeting", Greeting.class);
+		ApplicationContext context = SpringApplication.run(GreetingAnnotationConfig.class);
+		Greeting greeting = context.getBean("greetingImpl", Greeting.class);
+		String message = greeting.getMessage();
+		System.out.println("message : " + message);
+		assertEquals("Welcome to Spring", message);
+	}
+	
+	/**
+	 * Bean defined in a particular @Profile should not be available until profile is set "spring.profiles.active"
+	 */
+	@Test(expected=NoSuchBeanDefinitionException.class) 
+	public void testProfileBeanWithoutSettingProfile()
+	{
+		//Helps in removing the profile property set in other tests if any
+		System.clearProperty("spring.profiles.active");
+		
+		System.out.println("AnnotationConfigTest.testProfileBeanWithoutSettingProfile() : " + System.getProperty("spring.profiles.active"));
+		
+		ApplicationContext context2 = SpringApplication.run(GreetingAnnotationConfig.class);
+		Greeting greeting = context2.getBean("greetingImpl2", Greeting.class);
+		System.out.println(greeting.getMessage());
+		fail("Should not have reached here");
+	}
+	
+
+	/**
+	 * Bean defined in a particular @Profile should  be available when profile is set "spring.profiles.active"
+	 */
+	@Test 
+	public void testProfileBeanWithSettingProfile()
+	{
+		System.setProperty("spring.profiles.active", "test1");
+		
+		System.out.println("AnnotationConfigTest.testProfileBeanWithSettingProfile() : " + System.getProperty("spring.profiles.active"));
+		
+		
+		ApplicationContext context2 = SpringApplication.run(GreetingAnnotationConfig.class);
+		Greeting greeting = context2.getBean("greetingImpl1", Greeting.class);
 		String message = greeting.getMessage();
 		assertEquals("Welcome to Spring, Nirmal Davis", message);
-	}	
+	}
+
+	@Test(expected=NoUniqueBeanDefinitionException.class)
+	public void testNoUniqueBeanException() {
+		System.setProperty("spring.profiles.active", "test1");
+		
+		ApplicationContext context2 = SpringApplication.run(GreetingAnnotationConfig.class);
+		Greeting greeting = context2.getBean(Greeting.class);
+		System.out.println(greeting.getMessage());
+		fail("Should not have reached here");
+		
+	}
+	
+	@Test 
+	public void testFiledInjection()
+	{
+		System.setProperty("spring.profiles.active", "test2");
+		
+		ApplicationContext context2 = SpringApplication.run(GreetingAnnotationConfig.class);
+		Greeting greeting = context2.getBean("greetingImpl2",Greeting.class);
+		String message = greeting.getMessage();
+		assertEquals("Welcome to Spring, Nirmal Davis", message);
+	}
+
+	@Test 
+	public void testConstructorInjection()
+	{
+		System.setProperty("spring.profiles.active", "test3");
+		
+		ApplicationContext context2 = SpringApplication.run(GreetingAnnotationConfig.class);
+		Greeting greeting = context2.getBean("greetingImpl3",Greeting.class);
+		String message = greeting.getMessage();
+		assertEquals("Welcome to Spring, Nirmal Davis", message);
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void testRequiredFalseInFieldInjection()
+	{
+		System.setProperty("spring.profiles.active", "test4");
+		
+		ApplicationContext context2 = SpringApplication.run(GreetingAnnotationConfig.class);
+		Greeting greeting = context2.getBean("greetingImpl4",Greeting.class);
+		String message = greeting.getMessage();
+		System.out.println("AnnotationConfigTest.testRequiredFalseInFieldInjection() - message : " + message);
+		assertEquals("Welcome to Spring, Nirmal Davis", message);
+	}
+
+	@Test
+	public void testRequiredFalseInFieldInjectionWorking()
+	{
+		System.setProperty("spring.profiles.active", "test5");
+		
+		ApplicationContext context2 = SpringApplication.run(GreetingAnnotationConfig.class);
+		Greeting greeting = context2.getBean("greetingImpl5",Greeting.class);
+		String message = greeting.getMessage();
+		System.out.println("AnnotationConfigTest.testRequiredFalseInFieldInjection() - message : " + message);
+		assertEquals("Welcome to Spring", message);
+	}
+	
+	@Test 
+	public void testRequiredFalseInConstructorInjection()
+	{
+		System.setProperty("spring.profiles.active", "test6");
+		
+		ApplicationContext context2 = SpringApplication.run(GreetingAnnotationConfig.class);
+		Greeting greeting = context2.getBean("greetingImpl6",Greeting.class);
+		String message = greeting.getMessage();
+		System.out.println("AnnotationConfigTest.testRequiredFalseInConstructorInjection() - message : " + message);
+		assertEquals("Welcome to Spring", message);
+	}
+	
+	
+	
+	@Test
+	public void testOptionalWithAddress() {
+		System.setProperty("spring.profiles.active", "test11");
+		
+		ApplicationContext context2 = SpringApplication.run(GreetingAnnotationConfig.class);
+		Greeting greeting = context2.getBean("Greeting",Greeting.class);
+		String message = greeting.getMessage();
+		System.out.println("AnnotationConfigTest.testOptionalWithAddress() - message : " + message);
+		assertEquals("Welcome to Spring, Nirmal Davis @ Kundalahalli Gate", message);
+	}
+	
+
+	@Test 
+	public void testOptionalWithoutAddress() {
+		System.setProperty("spring.profiles.active", "test12");
+		
+		ApplicationContext context2 = SpringApplication.run(GreetingAnnotationConfig.class);
+		Greeting greeting = context2.getBean("Greeting",Greeting.class);
+		String message = greeting.getMessage();
+		System.out.println("AnnotationConfigTest.testOptionalWithoutAddress() - message : " + message);
+		assertEquals("Welcome to Spring, Nirmal Davis", message);
+	}
+
+	@Test
+	public void testOptionalWithQualifiedAddress() {
+		System.setProperty("spring.profiles.active", "test13");
+		
+		ApplicationContext context2 = SpringApplication.run(GreetingAnnotationConfig.class);
+		Greeting greeting = context2.getBean("Greeting",Greeting.class);
+		String message = greeting.getMessage();
+		System.out.println("AnnotationConfigTest.testOptionalWithQualifiedAddress() - message : " + message);
+		assertEquals("Welcome to Spring, Nirmal Davis @ White Field", message);
+	}
+
+	
+	@Test @Ignore
+	public void testAutoWiredInComponent() {
+//		System.setProperty("spring.profiles.active", "test");
+		ApplicationContext context2 = SpringApplication.run(GreetingAnnotationConfig.class);
+		Greeting greeting = context2.getBean("greetingImpl2", Greeting.class);
+		String message = greeting.getMessage();
+		System.out.println("message : " + message);
+		
+		assertEquals("Welcome to Spring, nirmal Davis", message);
+		
+	}
 
 }
